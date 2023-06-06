bab61368109d33f13c421bc71d723f3027206a46
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-06-06 10:46:38 -0600

    Added a NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return(
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3> Weather DemoAPP</h3>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
